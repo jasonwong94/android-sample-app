d84546ac75e5a9b7f7f41ffe08c54a5bfbd80fef
Jason Wong <jaw.wong@mail.utoronto.ca>
2015-11-15 19:16:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jasonwong94/productapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.jasonwong94.productapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText titleText ,descriptionText ,costText, quantityText, totalCostText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_product);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+
+
+        titleText = (EditText) findViewById( R.id.productTitle );
+        descriptionText = (EditText) findViewById( R.id.productDescription );
+        costText = (EditText) findViewById( R.id.productCost );
+        quantityText = (EditText) findViewById( R.id.productQuantity );
+        totalCostText = (EditText) findViewById( R.id.productTotalCost );
+
+        costText.addTextChangedListener( calculateWatcher );
+        quantityText.addTextChangedListener( calculateWatcher );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void calculateCost(){
+        String costString = costText.getText().toString();
+        String quantityString = quantityText.getText().toString();
+
+        if( !costString.isEmpty() && !quantityString.isEmpty() ){
+//            calculate cost only if both parameters have a numeric value (inputType: number )
+            float cost = Float.valueOf ( costString );
+            int quantity = Integer.valueOf( quantityString );
+
+            float totalCost = cost * quantity;
+
+            totalCostText.setText( String.valueOf( totalCost ) );
+        }
+    };
+
+    private final TextWatcher calculateWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if( s.length() > 0 ){
+                calculateCost();
+            }
+        }
+    };
+
+    public void reset( View view ){
+//        clears out the form
+        titleText.setText( "" );
+        descriptionText.setText( "" );
+        costText.setText( "" );
+        quantityText.setText( "" );
+        totalCostText.setText( "" );
+    }
+}
