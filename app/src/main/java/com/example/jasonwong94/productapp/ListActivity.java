3e5acd05d423bb7444d7f57ebaba7ce93f452836
Jason Wong <jaw.wong@mail.utoronto.ca>
2015-11-17 16:27:16 -0500

    Created activity for floating button to activate create product form

--- app/src/main/java/com/example/jasonwong94/productapp/ListActivity.java
+++ app/src/main/java/com/example/jasonwong94/productapp/ListActivity.java
@@ -3,7 +3,6 @@ package com.example.jasonwong94.productapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,7 +20,7 @@ public class ListActivity extends AppCompatActivity {
         fab.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick( View view ){
-                Intent intent = new Intent( ListActivity.this, MainActivity.class );
+                Intent intent = new Intent( ListActivity.this, CreateProductActivity.class );
                 startActivity( intent );
             }
         });
